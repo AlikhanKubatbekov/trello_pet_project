6bfcaff6f78716bc5a76326abb6703fe4521c7c2
Alikhan Kubatbekov <alixan.kubatbekov@gmail.com>
2025-02-12 20:38:59 +0600

    Added types for frontend

--- backend/dist/time-block/time-block.controller.d.ts
+++ backend/dist/time-block/time-block.controller.d.ts
@@ -5,51 +5,51 @@ export declare class TimeBlockController {
     private readonly timeBlockService;
     constructor(timeBlockService: TimeBlockService);
     getAll(userId: string): Promise<{
+        name: string;
         id: string;
         createdAt: Date;
         updatedAt: Date;
         userId: string;
-        name: string;
         color: string | null;
         duration: number;
         order: number;
     }[]>;
     create(dto: TimeBlockDto, userId: string): Promise<{
+        name: string;
         id: string;
         createdAt: Date;
         updatedAt: Date;
         userId: string;
-        name: string;
         color: string | null;
         duration: number;
         order: number;
     }>;
     updateOrder(updateOrderDto: UpdateOrderDto): Promise<{
+        name: string;
         id: string;
         createdAt: Date;
         updatedAt: Date;
         userId: string;
-        name: string;
         color: string | null;
         duration: number;
         order: number;
     }[]>;
     update(dto: TimeBlockDto, userId: string, id: string): Promise<{
+        name: string;
         id: string;
         createdAt: Date;
         updatedAt: Date;
         userId: string;
-        name: string;
         color: string | null;
         duration: number;
         order: number;
     }>;
     delete(userId: string, id: string): Promise<{
+        name: string;
         id: string;
         createdAt: Date;
         updatedAt: Date;
         userId: string;
-        name: string;
         color: string | null;
         duration: number;
         order: number;
--- backend/dist/time-block/time-block.service.d.ts
+++ backend/dist/time-block/time-block.service.d.ts
@@ -4,51 +4,51 @@ export declare class TimeBlockService {
     private prisma;
     constructor(prisma: PrismaService);
     getAll(userId: string): Promise<{
+        name: string;
         id: string;
         createdAt: Date;
         updatedAt: Date;
         userId: string;
-        name: string;
         color: string | null;
         duration: number;
         order: number;
     }[]>;
     create(dto: TimeBlockDto, userId: string): Promise<{
+        name: string;
         id: string;
         createdAt: Date;
         updatedAt: Date;
         userId: string;
-        name: string;
         color: string | null;
         duration: number;
         order: number;
     }>;
     update(dto: Partial<TimeBlockDto>, timeBlockId: string, userId: string): Promise<{
+        name: string;
         id: string;
         createdAt: Date;
         updatedAt: Date;
         userId: string;
-        name: string;
         color: string | null;
         duration: number;
         order: number;
     }>;
     delete(timeBlockId: string, userId: string): Promise<{
+        name: string;
         id: string;
         createdAt: Date;
         updatedAt: Date;
         userId: string;
-        name: string;
         color: string | null;
         duration: number;
         order: number;
     }>;
     updateOrder(ids: string[]): Promise<{
+        name: string;
         id: string;
         createdAt: Date;
         updatedAt: Date;
         userId: string;
-        name: string;
         color: string | null;
         duration: number;
         order: number;
